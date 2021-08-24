f1185f7f21c6c4d7b5e5861a42f12130286756f6
Dylan Garrett <dylan.garrett@gmail.com>
2021-08-23 21:43:39 -0700

    Check pdf upload status after uploading

--- readwise-pdf-sync.py
+++ readwise-pdf-sync.py
@@ -3,17 +3,24 @@
 import requests
 import re
 import sys
+import time
 from secrets import Cookie
 
+start_time = int(time.time() * 1000)
+
 pdf_page_url = 'https://readwise.io/import/pdf/'
-api_url = "https://readwise.io/api/upload_misc_file"
+upload_api_url = 'https://readwise.io/api/upload_misc_file'
+result_api_url = 'https://readwise.io/api/sync_status'
 
 headers = {
     'Accept': 'application/json',
     'Cookie': Cookie,
-    'Referer': 'https://readwise.io/import/pdf/',
+    'Referer': 'https://readwise.io/',
 }
 
+#
+# Get the CSRF upload middleware token
+#
 pdf_page = requests.request('GET', pdf_page_url, headers=headers)
 csrf_search = re.search(r'name="csrfmiddlewaretoken" value="(.*)"', pdf_page.text, re.IGNORECASE)
 
@@ -24,11 +31,39 @@ if not csrf_search:
 csrf = csrf_search.group(1)
 print(csrf)
 
+#
+# Upload PDF
+#
 payload = {'file_type': 'pdf',
            'csrfmiddlewaretoken': csrf}
 files = [
     ('file', open(sys.argv[1], 'rb'))
 ]
-response = requests.request("POST", api_url, headers=headers, data = payload, files = files)
+upload_response = requests.request("POST", upload_api_url, headers=headers, data = payload, files = files)
+upload_response_json = upload_response.json()
 
-print(response.text.encode('utf8'))
+print(upload_response.text)
+
+#
+# Check upload status
+#
+params = {'syncsAfter': start_time,
+          'fileID': upload_response_json['file_id']}
+
+status_attempts = 10
+result_response_json = None
+while not result_response_json and status_attempts:
+    result_response = requests.request('GET', result_api_url, headers=headers, params=params)
+    res_json = result_response.json()
+    if res_json['isFinished']:
+        result_response_json = res_json
+    status_attempts = status_attempts - 1
+
+if not result_response_json:
+    print('Processing book failed')
+    exit(-1)
+
+num_books = len(result_response_json["userBooks"])
+print(f'Uploaded {num_books} book(s)')
+for book in result_response_json['userBooks']:
+    print(f'Finished uploading "{book["book_data__title"]}" by "{book["book_data__author"]}"')
